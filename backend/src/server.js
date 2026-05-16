ba4c00ccb9274a8c4720e5670f9539db940a23af
Kothavale Arya <aryakothavale04@gmail.com>
2026-05-16 11:23:27 +0530

    Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,25 @@ import { connectDb } from "./config/db.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const startServer = async () => {
+  try {
+    const port = process.env.PORT || 5000;
 
-await connectDb();
+    console.log("Connecting MongoDB...");
+    await connectDb();
 
-createApp().listen(port, () => {
-  console.log(`ScanApp API running on http://localhost:${port}`);
-});
+    console.log("Creating Express App...");
+    const app = createApp();
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+
+  } catch (error) {
+    console.error("SERVER ERROR:");
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
